f979d1f77f47a8ccf787170afd335ed1b9a18cce
sophxmia <maliarenko.sofiia@gmail.com>
2024-01-25 16:35:17 +0200

    Initialize project

---
+++ src/main/java/patterns/example/Main.java
@@ -0,0 +1,20 @@
+package patterns.example;
+
+import java.util.List;
+
+import static patterns.example.Movie.MovieType.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Rental> rentals = List.of(new Rental(new Movie("Rambo", REGULAR), 1),
+                new Rental(new Movie("Lord of the Rings", NEW_RELEASE), 4),
+                new Rental(new Movie("Harry Potter", CHILDRENS), 5));
+
+        Customer customer = new Customer("John Doe", rentals);
+        String statement = customer.statement();
+
+        System.out.println(statement);
+    }
+}
